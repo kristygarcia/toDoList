dfccd5c70762d049161ba7f4a66511ec9e45765f
Kristy Garcia <kristymichgarcia@gmail.com>
2023-09-09 19:04:15 -0400

    Initial Commit

---
+++ toDoList/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  toDoList
+//
+//  Created by Kristy Garcia on 9/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ toDoList/toDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  toDoListApp.swift
+//  toDoList
+//
+//  Created by Kristy Garcia on 9/9/23.
+//
+
+import SwiftUI
+
+@main
+struct toDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
